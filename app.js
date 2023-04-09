8481704d88792cf3fc57468afc9804accddd5686
SirDon3 <110694491+SirDon3@users.noreply.github.com>
2023-04-09 23:20:49 +0300

    API data fetched
    Weather data fetched from open weather map. currently the location data is being extracted and displayed in the front end

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// State 
+
+let currentCity = "Helsinki"
+let units = "metric"
+
+// Selectors
+
+let city = document.querySelector(".weather_city")
+
+
 function getWeather(){
-    
+    const API_Key = '64f60853740a1ee3ba20d0fb595c97d5'
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${API_Key}&units=${units}`).then(res => res.json())
+    .then(data => {
+        city.innerHTML =`${data.name}, ${data.sys.country}`
+    })
 }
+
+document.body.addEventListener("load", getWeather())
