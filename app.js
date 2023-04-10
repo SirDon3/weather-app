dac73063e4735b2ee7d7b92ddc8ac762ce74d17b
SirDon3 <110694491+SirDon3@users.noreply.github.com>
2023-04-10 19:00:49 +0300

    update app.js
    Facing issue with converting time zone function.

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
 // State 
 
-let currentCity = "Helsinki"
+let currentCity = "Kandy"
 let units = "metric"
 
 // Selectors
 
 let city = document.querySelector(".weather_city")
+let datetime = document.querySelector(".weather_datetime")
+
+
+
+// function convertTimeStamp(timestamp, timezone){
+//     const convertTimezone = timezone / 3600 // convert seconds to hours 
+
+//     const date = new Date(timestamp * 1000)
+   
+//     const options = {
+//        weekday: "long",
+//        day: "numeric",
+//        month: "long",
+//        year: "numeric",
+//        hour: "numeric",
+//        minute: "numeric",
+//        timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
+//        hour12: true,
+//    }
+
+//    return date.toLocaleString("en-US", options) // 
+
+
+// FACING ISSUE Uncaught (in promise) RangeError: Invalid time zone specified: Etc/GMT-5.5)
+// Unable to reslove
+  
+// }
+
+
+
+
+// convert country code to name
+
+function convertCountryCode(country){
+    let regionName = new Intl.DisplayNames(["en"], {type: "region"});
+    return regionName.of(country)
+}
 
 
 function getWeather(){
-    const API_Key = '64f60853740a1ee3ba20d0fb595c97d5'
+    const API_Key = '59618624f2e4aeffe559c95b41d84083'
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${API_Key}&units=${units}`).then(res => res.json())
     .then(data => {
-        city.innerHTML =`${data.name}, ${data.sys.country}`
+        console.log(data)
+        city.innerHTML =`${data.name}, ${convertCountryCode(data.sys.country)}`
+        datetime.innerHTML = convertTimeStamp(data.dt, data.timezone)
     })
 }
 
